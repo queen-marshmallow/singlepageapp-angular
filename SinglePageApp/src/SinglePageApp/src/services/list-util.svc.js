ac998db66bc08a5bc7aa1b869a06d6ae09f2a839
queen-marshmallow <fletcher.madeleine@gmail.com>
2015-08-17 21:47:02 -0700

    Collapsable job list and links in dashboard.

---
+++ SinglePageApp/src/SinglePageApp/src/services/list-util.svc.js
@@ -0,0 +1,16 @@
+﻿ngm('app').factory('listUtilSvc', [function () {
+    return {
+        groupBy: function(accessor, list) {
+            var grouped = {};
+
+            angular.forEach(list, function (item, key) {
+                if (!grouped[accessor(item)]) {
+                    grouped[accessor(item)] = [];
+                }
+                grouped[accessor(item)].push(item);
+            });
+
+            return grouped;
+        }
+    };
+}]);
